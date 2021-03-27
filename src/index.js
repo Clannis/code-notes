eb93cb8211bdc43adb8d793b62f591cb60a81c01
clannis <dodds1990@gmail.com>
2021-03-27 16:30:18 -0500

    Create titlebar to allow window to be dragged around the screen with app name embedded. Add styling to window

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import { AppName } from '../appConfig'
+
+let titleBar = document.createElement('div')
+
+titleBar.className = 'titlebar'
+document.body.appendChild(titleBar)
+
+let appTitle = document.createElement('h1')
+appTitle.className = 'titlebar__title'
+appTitle.innerText = `${AppName}`
+titleBar.appendChild(appTitle)
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div')
